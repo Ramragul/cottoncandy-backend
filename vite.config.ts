afa30e7d2396c4ee25911885b509438ffa2981bb
Ramragul <iotprograms@gmail.com>
2024-07-28 18:10:08 +0530

    New Build

--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,25 @@
 
 // Version 2 , Vercel Prod env fix
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist', // Specify the output directory for the build files
+//   },
+// })
+
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Specify the output directory for the build files
+    outDir: 'dist', // Ensure the output directory matches the Vercel config
   },
-})
+});
