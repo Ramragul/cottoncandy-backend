c65eba039b58a52603c3cf61daa653b7762c1610
Ramragul <iotprograms@gmail.com>
2024-07-28 17:12:14 +0530

    vercel prod env issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+// Version 1
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+// Version 2 , Vercel Prod env fix
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory for the build files
+  },
 })
