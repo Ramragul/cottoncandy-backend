0f657b25ea25f8cded7090e8bfa2b6d32c07dfbd
Ramragul <iotprograms@gmail.com>
2026-02-11 13:36:04 +0530

    mobile app with tailoring version changes

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.co.cottoncandy.app',
+  appName: 'Cotton Candy',
+  webDir: 'dist'
+};
+
+export default config;
